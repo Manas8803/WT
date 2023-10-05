add48c9224dbe7f7a49338d0c48c447aa438c7f9
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2023-10-05 12:06:53 +0530

    Changed to Html

--- src/Pages/Vans/Index.jsx
+++
@@ -1,4 +0,0 @@
-import { VanDetail, VDloader } from "./VanDetail";
-import { Vans, vansLoader } from "./Vans";
-
-export { VanDetail, Vans, vansLoader, VDloader };
--- src/Utility/index.jsx
+++
@@ -1,5 +0,0 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import VanCard from "./VanCard";
-
-export { Navbar, Footer, VanCard };
